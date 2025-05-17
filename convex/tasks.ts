af062fab53836033ec07fad116b36edf4aafaaa5
kolaborateplatform <mulikateteangella@gmail.com>
2025-05-17 12:22:35 -0700

    added update api

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -58,4 +58,34 @@ export const search = query({
     }
 });
 
+export const update = mutation ({
+    args: {
+        id: v.id('tasks'),
+        title: v.string(),
+        description: v.string(),
+        creatorId: v.id('users'),
+        assigneeId: v.id('users'),
+        dueDate: v.optional(v.number()),
+        status: v.union(v.literal('todo'), v.literal('in-progress'), v.literal('done')),
+        attachments: v.array(v.id("_storage")),
+    },
 
+    handler: async(ctx, args) => {
+        const { id, ...updates } = args;
+        const userId = await getAuthUserId(ctx);
+        if (!userId) {
+            throw new Error('User not authenticated');
+        }
+
+        const task = await ctx.db.get(args.id);
+        if (!task) {
+            throw new Error('Task not found');
+        }
+
+        if (task.creatorId !== userId) {
+            throw new Error('User not authorized to update this task');
+        }
+
+        await ctx.db.patch(id, updates);
+    }
+});
